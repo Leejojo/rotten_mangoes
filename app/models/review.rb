5977565954ebf3cdc1bedd525cb13acb5b971572
Leejojo <noodle.lee@gmail.com>
2016-07-19 13:19:10 -0400

    space test

--- app/models/review.rb
+++ app/models/review.rb
@@ -17,5 +17,6 @@ class Review < ApplicationRecord
     numericality: { greater_than_or_equal_to: 1 }
   validates :rating_out_of_ten,
     numericality: { less_than_or_equal_to: 10 }
+   
     
 end
